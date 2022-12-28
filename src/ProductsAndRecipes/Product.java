5b935af45c117d00eb76b24845747e6fe30ad0eb
aleksandr <drayvol@gmail.com>
2022-12-29 02:21:34 +0300

    hw

--- src/ProductsAndRecipes/Product.java
+++ src/ProductsAndRecipes/Product.java
@@ -35,8 +35,8 @@ public class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Objects.equals(name, product.name)
-
+        return Objects.equals(name, product.name);
+    }
     @Override
     public int hashCode() {
         return Objects.hash(name);
